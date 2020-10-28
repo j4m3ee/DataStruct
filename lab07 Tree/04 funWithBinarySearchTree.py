59a7c261c242653f601ae81a56e93a45f447702c
j4m3ee <j4m3ee01@gmail.com>
2020-10-28 22:00:18 +0700

    commit

--- lab07 Tree/04 funWithBinarySearchTree.py
+++ lab07 Tree/04 funWithBinarySearchTree.py
@@ -9,22 +9,6 @@ class Node:
     def __str__(self):
         return str(self.data)
 
-class Stack:
-    def __init__(self):
-        self.item = []
-
-    def isEmpty(self):
-        return self.item == []
-
-    def size(self):
-        return len(self.item)
-
-    def push(self,data):
-        self.item.append(data)
-
-    def pop(self):
-        return self.item.pop() if not self.isEmpty() else None
-
 class Queue:
     def __init__(self):
         self.item = []
@@ -82,22 +66,40 @@ class BST:
                 q.enQ(n.right)
         return ls
 
+
     def inOrder(self,root):
-        return (self.inOrder(root.left) + [root.data] + self.inOrder(root.right)) if root else []
+        if root != None:
+            
+            self.inOrder(root.left)
+            print(root.data,end = ' ')
+            self.inOrder(root.right)
         
     def postOrder(self,root):
-        return (self.postOrder(root.left) + self.postOrder(root.right) + [root.data] ) if root else []
+        if root != None:
+            self.postOrder(root.left)
+            self.postOrder(root.right)
+            print(root.data,end = ' ')
 
     def preOrder(self,root):
-        return ([root.data] + self.preOrder(root.left) + self.preOrder(root.right)) if root else []
+        if root != None:
+            print(root.data,end = ' ')
+            self.preOrder(root.left)
+            self.preOrder(root.right)
 
 
 T = BST()
 inp = [int(i) for i in input('Enter Input : ').split()]
 for i in inp:
     root = T.insert(i)
-print('Preorder :',' '.join(str(i) for i in T.preOrder(root)))
-print('Inorder :',' '.join(str(i) for i in T.inOrder(root)))
-print('Postorder :',' '.join(str(i) for i in T.postOrder(root)))
+
+print('Preorder :',end = ' ')
+T.preOrder(root)
+print()
+print('Inorder :',end = ' ')
+T.inOrder(root)
+print()
+print('Postorder :',end = ' ')
+T.postOrder(root)
+print()
 print('Breadth :',' '.join(str(i) for i in T.breadthFirst()))
 
